8cb80b56f9f0c7abe6ce4be1dcd9ed88b4fd2dd9
Jailson Zarur <jailsoncardoz123@gmail.com>
2025-05-13 10:05:58 -0300

    feat: criação dos schemas de request para o controller. #1

---
+++ api/src/infra/http/schemas/auth.ts
@@ -0,0 +1,24 @@
+import { z } from "zod";
+
+export const signUpRequestBodySchema = z.object({
+	name: z.string().min(3, "O Nome deve ter no mínimo 3 caracteres"),
+	email: z.string().email("O Email inválido"),
+	password: z.string().min(8, "A senha deve ter no mínimo 8 caracteres"),
+	phone: z.string().refine(
+		(val) => {
+			const length = val.length;
+			return length === 10 || length === 11;
+		},
+		{
+			message: "Telefone inválido",
+		}
+	),
+	document: z.string().refine(
+		(val) => {
+			const length = val.length;
+			return length === 11 || length === 14;
+		},
+		{ message: "Documento Inválido" }
+	),
+});
+
