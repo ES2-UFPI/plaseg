a5e59dc6ab3f3c3cddd85c636cebead67e398747
Izaias Lima <izaiaslima356@gmail.com>
2025-05-13 09:47:08 -0300

    feat: setup da aplicação e tela de cadastro. closes #14

---
+++ web/src/pages/sign-in.tsx
@@ -0,0 +1,9 @@
+export default function SignIn() {
+	return (
+		<div className="w-full h-screen flex items-center justify-center">
+			<div className="w-full max-w-md bg-white rounded-lg shadow-md p-4">
+				<h1 className="text-2xl font-bold">Sign In</h1>
+			</div>
+		</div>
+	);
+}
