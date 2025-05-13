e659c38949db97b6454b18616341cbc51aea3673
thalesapenas <thales.rodrigues@ufpi.edu.br>
2025-05-13 12:07:10 -0300

    feat: criação dos middlewares de autenticação. #3

---
+++ api/src/infra/http/middleware/verify-user-role.ts
@@ -0,0 +1,25 @@
+import { FastifyReply, FastifyRequest } from "fastify";
+
+export function verifyUserRole(roleToVerify: "ADMIN" | "MUNICIPALITY") {
+	return async (request: FastifyRequest, reply: FastifyReply) => {
+		try {
+			await request.jwtVerify();
+
+			const role = request.user.role;
+
+			if (role !== roleToVerify) {
+				return reply.status(401).send({
+					success: false,
+					errors: ["Usuário não autorizado"],
+					data: null,
+				});
+			}
+		} catch (err) {
+			return reply.status(401).send({
+				success: false,
+				errors: ["Usuário não autorizado"],
+				data: null,
+			});
+		}
+	};
+}
