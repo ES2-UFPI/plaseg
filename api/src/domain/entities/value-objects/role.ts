64929561cf68ed5c4346bbdbab28b52b665e5cfa
Jailson Zarur <jailsoncardoz123@gmail.com>
2025-05-20 08:35:29 -0300

    feat: adição desse novo tipo de usuário no value objects e sua verificação no verifyUserRole. #9

--- api/src/domain/entities/value-objects/role.ts
+++ api/src/domain/entities/value-objects/role.ts
@@ -3,7 +3,11 @@ import { Role as PrismaRole } from "@prisma/client";
 export enum DomainRole {
 	ADMIN = "ADMIN",
 	MUNICIPALITY = "MUNICIPALITY",
+	COMPANY = "COMPANY",
+	ADMIN_MASTER = "ADMIN_MASTER",
 }
+
+export type RoleType = "ADMIN" | "MUNICIPALITY" | "COMPANY" | "ADMIN_MASTER";
 
 export class Role {
 	private readonly value: DomainRole;
@@ -22,6 +26,8 @@ export class Role {
 				return PrismaRole.ADMIN;
 			case DomainRole.MUNICIPALITY:
 				return PrismaRole.MUNICIPALITY;
+			case DomainRole.ADMIN_MASTER:
+				return PrismaRole.ADMIN_MASTER;
 			default:
 				throw new Error(`Invalid role: ${this.value}`);
 		}
@@ -35,7 +41,30 @@ export class Role {
 		return new Role(DomainRole.MUNICIPALITY);
 	}
 
+	public static company(): Role {
+		return new Role(DomainRole.COMPANY);
+	}
+
+	public static adminMaster(): Role {
+		return new Role(DomainRole.ADMIN_MASTER);
+	}
+
 	public toString(): string {
 		return this.value;
+	}
+
+	public static fromString(role: string): Role {
+		switch (role) {
+			case DomainRole.ADMIN:
+				return Role.admin();
+			case DomainRole.MUNICIPALITY:
+				return Role.member();
+			case DomainRole.COMPANY:
+				return Role.company();
+			case DomainRole.ADMIN_MASTER:
+				return Role.adminMaster();
+			default:
+				throw new Error(`Invalid role: ${role}`);
+		}
 	}
 }
--- api/src/infra/http/middleware/verify-user-role.ts
+++ api/src/infra/http/middleware/verify-user-role.ts
@@ -1,23 +1,31 @@
 import { FastifyReply, FastifyRequest } from "fastify";
+import { Role, RoleType } from "../../../domain/entities/value-objects/role";
 
-export function verifyUserRole(roleToVerify: "ADMIN" | "MUNICIPALITY") {
+/**
+ * Middleware para verificar se o usuário tem a role especificada
+ * @param rolesToVerify - A role que o usuário deve ter
+ * @returns Um middleware que verifica se o usuário tem a role especificada
+ */
+export function verifyUserRole(rolesToVerify: RoleType[]) {
 	return async (request: FastifyRequest, reply: FastifyReply) => {
 		try {
 			await request.jwtVerify();
 
 			const role = request.user.role;
 
-			if (role !== roleToVerify) {
+			const roleObject = Role.fromString(role);
+
+			if (!rolesToVerify.includes(roleObject.getValue())) {
 				return reply.status(401).send({
 					success: false,
-					errors: ["Usuário não autorizado"],
+					errors: ["Não autorizado"],
 					data: null,
 				});
 			}
 		} catch (err) {
 			return reply.status(401).send({
 				success: false,
-				errors: ["Usuário não autorizado"],
+				errors: ["Não autorizado"],
 				data: null,
 			});
 		}
