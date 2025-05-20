44253eefe3b00c81e813fff8dc62cf752c2620ad
Izaias Lima <izaiaslima356@gmail.com>
2025-05-20 09:18:13 -0300

    feat: estrutura de páginas reformulada. #11

---
+++ web/src/pages/admin/admin-dashboard.tsx
@@ -0,0 +1,7 @@
+export default function AdminDashboard() {
+	return (
+		<div className="w-full h-screen flex items-center justify-center">
+			<strong className="text-2xl font-extrabold">Dashboard do Admin</strong>
+		</div>
+	);
+}
