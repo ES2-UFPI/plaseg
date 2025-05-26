8ed53b4f2dc789b3232e2cc181518eddffb9a790
Jailson Zarur <jailsoncardoz123@gmail.com>
2025-05-26 17:26:03 -0300

    feat: criação da controller e seu teste e2e. closes #15

--- api/src/infra/http/controllers/products/products.routes.ts
+++ api/src/infra/http/controllers/products/products.routes.ts
@@ -1,8 +1,10 @@
 import { FastifyInstance } from "fastify";
 import { createBaseProduct } from "./create-base-product.controller";
 import { getBaseProduct } from "./get-base-products.controller";
+import { createSpecificProduct } from "./create-specific-product.controller";
 
 export async function productsRoutes(app: FastifyInstance) {
 	app.register(createBaseProduct);
 	app.register(getBaseProduct);
+	app.register(createSpecificProduct);
 }
